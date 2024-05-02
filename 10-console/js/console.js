9a12336abfa2011bebefb6d7bb3a3bc003afbb88
Souhailimohamed <souhaili.med86@gmail.com>
2024-05-02 12:38:12 -0300

    modification console.js

--- 10-console/js/console.js
+++ 10-console/js/console.js
@@ -12,23 +12,29 @@
 
 'use strict'
 
-const temperature = 21.3
-const fruits = ['pomme', 'poire', 'orange']
+const temperature = 21.3;
+const fruits = ['pomme', 'poire', 'orange'];
+
+let a=18;
+let b="dis-huit";
+let c=true;
+let d=null;
+let e;
 
 console.log('Salut.');
 console.log(temperature);
 console.log(fruits);
 console.info('La temperature est %f', temperature);
 console.info('la collection de fruits est %o',fruits);
-console.dirxml("<h1> pas d'interpreteur HTML </h1>")
-console.log(18, "dis-huit", true, null, undefined);
+console.dirxml("<h1> pas d'interpreteur HTML </h1>");
+console.log(a, b, c, d, e);
 
 
-console.group('Ceci est un groupe a part')
-console.log('Contenu du groupe')
-console.groupEnd()
+console.group('Ceci est un groupe a part');
+console.log('Contenu du groupe');
+console.groupEnd();
 
-console.group('Ceci est un second groupe a part (ferme par defaut)')
-console.log('Contenu du second groupe')
-console.groupEnd()
+console.group('Ceci est un second groupe a part (ferme par defaut)');
+console.log('Contenu du second groupe');
+console.groupEnd();
 
