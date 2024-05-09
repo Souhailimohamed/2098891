550349152cd5055fd5742a95fc34a83e9234451b
Souhailimohamed <souhaili.med86@gmail.com>
2024-05-09 11:39:10 -0300

    updates js and html page

--- Devoirs_Exercices_semaine_2/Exercices/04-Exercice/js/code.js
+++ Devoirs_Exercices_semaine_2/Exercices/04-Exercice/js/code.js
@@ -6,5 +6,7 @@ alert("Bienvenue à " + titre + " " + nom + " !");
 
 
 
+bienvenue ("M.", "Godin"); // message d'alerte : Bienvenue à M. Godin !
 
+bienvenue ("Mme.", "Albert"); // message d'alerte : Bienvenue à Mme. Albert !
 
