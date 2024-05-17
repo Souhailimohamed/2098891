882a1da968a14aba5f6695c0e0481c7b21a8a75e
Souhailimohamed <souhaili.med86@gmail.com>
2024-05-17 14:48:36 -0300

    mes exercices 08-13

---
+++ Devoirs_Exercices_semaine_3/Exercices/13-Exercice/js/code.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 5; i++) {
+
+  if (i == 2) {
+    //Je modifie certaines variables dans le programme lorsque i = 2 seulement;
+    continue;
+  }
+
+  document.write("<br> i est : " + i);
+}
